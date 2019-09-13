434f7ddf6c12e566deee9d1c854ebc0b9a79b342
ibayun <i.korniyenko@list.ru>
2019-09-13 11:31:37 +0300

    working bot

--- bot_games.py
+++ bot_games.py
@@ -7,20 +7,48 @@ TOKEN = "925351426:AAHzTYMJXTZOxlqlohzdF-IEJKpUKyfOIuk"
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(content_types=["test"])
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет.'
+                                      ' Опять ты меня разбудил. '
+                                      'Или не ты... Сейчас не об этом. '
+                                      'Ты ведь тут в картишки перебросить? '
+                                      'Тогда нажимай - /start_games !'
+                                      'Если забыл правила - /rules (специально для тебя собрал')
+
+
+@bot.message_handler(commands=["start_games"])
 def handle_answer(message):
-    print(message.text)
     bot.send_message(
-        message.chat.id, message.text
+        message.chat.id, "должна начаться игра"
+    )
+
+
+@bot.message_handler(commands=["rules"])
+def handle_answer(message):
+    bot.send_message(
+        message.chat.id, "правила:"
+    )
+
+
+@bot.message_handler(commands=["help"])
+def handle_answer(message):
+    bot.send_message(
+        message.chat.id, "Бот создан для игры в блэк джек. "
+                         "Чтобы начать игру нажми - /start_games,"
+                         " чтобы вспомнить правила игры - /rules,"
+                         " получить помощь - /help (Сейчас ты читаешь содержимое раздела помощи."
+                         " И, если понятнее не стало, обращайся сюда @AlexKhaliman, или сюда @i_bayun)"
     )
 
 
 @bot.message_handler(content_types=["text"])
 def handle_answer(message):
-    print(dir(message.text))
-    bot.send_message(
-        message.chat.id, message.text
-    )
+    if "/" in  message.text:
+        # print(dir(message.text), "- не существует. Попробуй /help")
+        bot.send_message(message.chat.id, "{} - комманда не существует. Попробуй /help".format(message.text))
+    else:
+        pass
 
 
 if __name__ == "__main__":
