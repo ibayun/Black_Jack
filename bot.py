596a58a76f349480a473f7506cc982a13226040f
ibayun <i.korniyenko@list.ru>
2019-09-13 11:33:59 +0300

    working bot bot.py

--- bot.py
+++ bot.py
@@ -9,47 +9,47 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне команду /start')
+    bot.send_message(message.chat.id, 'Привет.'
+                                      ' Опять ты меня разбудил. '
+                                      'Или не ты... Сейчас не об этом. '
+                                      'Ты ведь тут в картишки перебросить? '
+                                      'Тогда нажимай - /start_games !'
+                                      'Если забыл правила - /rules (специально для тебя собрал')
 
-"""@bot.message_handler(content_types=["start_games"])
+
+@bot.message_handler(commands=["start_games"])
 def handle_answer(message):
-    print(dir(message.text))
     bot.send_message(
         message.chat.id, "должна начаться игра"
     )
 
-@bot.message_handler(content_types=["rules"])
+
+@bot.message_handler(commands=["rules"])
 def handle_answer(message):
-    print(dir(message.text))
     bot.send_message(
         message.chat.id, "правила:"
     )
-"""
 
-"""@bot.message_handler(content_types=["text"])
+
+@bot.message_handler(commands=["help"])
 def handle_answer(message):
-    print(dir(message.text))
     bot.send_message(
-        message.chat.id, message.text
+        message.chat.id, "Бот создан для игры в блэк джек. "
+                         "Чтобы начать игру нажми - /start_games,"
+                         " чтобы вспомнить правила игры - /rules,"
+                         " получить помощь - /help (Сейчас ты читаешь содержимое раздела помощи."
+                         " И, если понятнее не стало, обращайся сюда @AlexKhaliman, или сюда @i_bayun)"
     )
 
-    if message.text == "Привет":
-    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-    bot.send_message(message.from_user.id, "Напиши привет")else:
-    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-    """
+
+@bot.message_handler(content_types=["text"])
+def handle_answer(message):
+    if "/" in  message.text:
+        # print(dir(message.text), "- не существует. Попробуй /help")
+        bot.send_message(message.chat.id, "{} - комманда не существует. Попробуй /help".format(message.text))
+    else:
+        pass
 
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
-"""@bot.message_handler(commands=['help'])
-def help_command(message):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add( telebot.types.InlineKeyboardButton( 'Message the developer', url='telegram.me/artiomtb' ) )
-    bot.send_message( message.chat.id, '1) To receive a list of available currencies press /exchange.\n' +
-    '2) Click on the currency you are interested in.\n' +
-    '3) You will receive a message containing information regarding the source and the target currencies, ' +
-                      'buying rates and selling rates.\n' + '4) Click “Update” to receive the current information regarding the request. ' +
-                      'The bot will also show the difference between the previous and the current exchange rates.\n' +
-                      '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.', reply_markup=keyboard )"""
